75aee9da6614979fcd957a7d5d8c8c9405aac759
z19ista09 <z19ista09@z00p2-09.SPSKNM>
2021-10-12 08:21:33 +0200

    Initial commit

---
+++ src/Student.java
@@ -0,0 +1,14 @@
+
+
+public class Student {
+
+    public static void main(String[] args) {
+        Student student1 = new Student();
+   System.out.println(student1.name + ": " + student1.numberOfStudent);
+    }
+
+    public String name = "John";
+    public int numberOfStudent = 12345;
+
+
+}
