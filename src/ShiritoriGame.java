666f67ac78555ae295a0c0b29c13d3fd659ad1d5
martinorion <martinhox03@gmail.com>
2021-10-21 08:52:21 +0200

    Add branch ShiritoriGame

--- src/ShiritoriGame.java
+++ src/ShiritoriGame.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class ShiritoriGame {
 
@@ -16,31 +17,35 @@ public class ShiritoriGame {
         this.gameOver = true;
     }
 
+   boolean over = true;
 
 
-
-    public List runGame(List<String> words){
+    public void runGame(List<String> words){
 
         for (int i = 0; i < words.size(); i++ ){
             if(play(words.get(i))){
                 alreadySaidWords.add(words.get(i));
+            System.out.println(alreadySaidWords);
+            }
+           else{
+               System.out.println("Game Over");
+               break;
+
             }
         }
        // cyklus cez words a v každom kroku cyklu zavolať play(word) a ak to vráti true potom si poznač povedané slovo
-       return words;
+
+
    }
 
-
-
-   public boolean play(String word){
+    public boolean play(String word){
        // vezmi posledné poznačené slovo a urob checky
        if (alreadySaidWords.isEmpty()) return true;
 
        String lastSaidWord = alreadySaidWords.get(alreadySaidWords.size()-1);
-       if (word.startsWith(String.valueOf(lastSaidWord.charAt(lastSaidWord.length() -1))))
+       if (word.toLowerCase(Locale.ROOT).startsWith(String.valueOf(lastSaidWord.charAt(lastSaidWord.length() -1)).toLowerCase(Locale.ROOT)) && !alreadySaidWords.contains(word)) {
            return true;
-
-       if (alreadySaidWords.contains(lastSaidWord));
+       }
        return false;
    }
 
@@ -50,8 +55,8 @@ public class ShiritoriGame {
         list.add("Mango");
         list.add("Obor");
         list.add("Rampa");
-        list.add("Asiminister");
+        list.add("nigga");
 
-        System.out.println();
+        new ShiritoriGame().runGame(list);
     }
 }
