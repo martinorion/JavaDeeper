1a5719d2dd48f6d242e7368524219d8b549a8ced
MartinHolena <martinkiko18@gmail.com>
2021-10-29 17:33:16 +0200

    Collections 7/10 (class Person rewrite)

--- src/Person.java
+++ src/Person.java
@@ -1,5 +1,6 @@
 import javax.sql.StatementEvent;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 public class Person {
@@ -8,17 +9,28 @@ public class Person {
     private int age;
     private int budget;
 
+
     public Person( String name1, int age1, int budget1){
         name = name1;
         age = age1;
         budget = budget1;
     }
 
-public static void sumBudget(ArrayList<Person> people){
+    public static void sumBudget(ArrayList<Person> people){
     ArrayList<Integer> budget2 = new ArrayList<>();
     people.forEach(i-> budget2.add(i.budget));
     Integer sumOfBudgets = budget2.stream().reduce(0, (Integer::sum));
     System.out.println("Suma budgetov je: " + sumOfBudgets);
+    }
+
+    public static boolean containA(ArrayList<Person> people){
+  ArrayList<Person> letter = new ArrayList<>();
+  people.stream().filter(a -> a.name.contains("a")).forEach(letter::add);
+
+  if(letter.isEmpty()){
+      return false;
+  }
+  return true;
     }
 
 
@@ -29,7 +41,7 @@ public static void sumBudget(ArrayList<Person> people){
         persones.add(new Person("Martin", 16, 2700));
 
         sumBudget(persones);
-
+        System.out.println(containA(persones));
         }
 
     }
