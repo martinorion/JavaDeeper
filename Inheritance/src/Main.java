eb4e0195eb06694042fe3485f7f27f74ffde98f5
martinorion <martinhox03@gmail.com>
2021-11-04 09:03:01 +0100

    Inheritance 4/5

--- Inheritance/src/Main.java
+++ Inheritance/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
         ArrayList<Employee> employee = new ArrayList<>();
         employee.add(new Teacher(1700,200));
-        employee.add(new Programmer(900,300));
+        employee.add(new Programmer(900,1000));
         employee.add(new Driver(1000, 65));
 
         employee.forEach(Employee::getInfo);
--- Inheritance/src/Programmer.java
+++ Inheritance/src/Programmer.java
@@ -3,4 +3,9 @@ public class Programmer extends Employee {
     public Programmer(float salary, int bonus){
         super(EmployeeType.Programmer, salary,bonus);
     }
+    private final float sumSalary = salary + bonus;
+
+    public void getInfo(){
+       System.out.println(employeeType + "’s salary is " + sumSalary);
+    }
 }
