d63447508efac1f48b8f117596f4bf97a8430401
martinorion <martinhox03@gmail.com>
2021-11-04 08:49:43 +0100

    Inheritance 3/5

--- Inheritance/src/Main.java
+++ Inheritance/src/Main.java
@@ -1,16 +1,17 @@
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args){
-        Teacher teacher = new Teacher(1700,200);
-        teacher.getInfo();
-
-        Programmer programmer = new Programmer(900,300);
-        programmer.getInfo();
-
-          Driver driver = new Driver(1000, 65);
-          driver.getInfo();
 
 
+        ArrayList<Employee> employee = new ArrayList<>();
+        employee.add(new Teacher(1700,200));
+        employee.add(new Programmer(900,300));
+        employee.add(new Driver(1000, 65));
+
+        employee.forEach(Employee::getInfo);
     }
+
 
 }
